54e753e75fe94463859e342533299360a7bb868d
Rkebede <rutht.kebede@gmail.com>
2020-04-03 11:29:57 -0500

    first commit

---
+++ spec/weather_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Weather do
+  it "has a version number" do
+    expect(Weather::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
